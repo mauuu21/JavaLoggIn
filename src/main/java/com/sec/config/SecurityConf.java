daa09f09d321bf89b521fb00421c28024f142310
Mate Morvai <mate.morvai@services4-it.eu>
2025-02-19 11:56:49 +0100

    Loggin change security deprecated parts

--- src/main/java/com/sec/config/SecurityConf.java
+++ src/main/java/com/sec/config/SecurityConf.java
@@ -1,31 +1,49 @@
 package com.sec.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableGlobalMethodSecurity(securedEnabled = true)
 @Configuration
-public class SecurityConf extends WebSecurityConfigurerAdapter {
+public class SecurityConf {
 	
 	@Autowired
 	public void configureAuth(AuthenticationManagerBuilder auth) throws Exception{
 		auth
 		  .inMemoryAuthentication()
-		    .withUser("sfjuser") 
+		    .withUser("User")
 		    .password("pass")
 		    .roles("USER")
 		   .and()
-             .withUser("sfjadmin")
+             .withUser("Admin")
              .password("pass")
              .roles("ADMIN");
 	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/admin/**")
+						.hasRole("ADMIN")		//admin felületet csak admin érhet el
+						.anyRequest().authenticated())				//mindent authentikálunk
+				.formLogin(config -> config
+						.loginPage("/login")
+						.permitAll())			//bárki elérheti
+				.logout(logout -> logout
+						.logoutUrl("/logout")
+						.logoutSuccessUrl("/login?logout")
+						.permitAll()
+				);
+		return http.build();
+	}
 	
-	@Override
+	/*@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
@@ -40,5 +58,5 @@ public class SecurityConf extends WebSecurityConfigurerAdapter {
 				.logoutSuccessUrl("/login?logout")
 				.permitAll();
 	}	
-	
+	*/
 }
